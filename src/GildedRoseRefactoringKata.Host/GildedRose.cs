ca77d51192f61f942ac1f397de83da5e3ed6b205
Morteza <mousavi310@gmail.com>
2021-12-13 19:50:52 +0330

    Add Inventory.

--- src/GildedRoseRefactoringKata.Host/GildedRose.cs
+++ src/GildedRoseRefactoringKata.Host/GildedRose.cs
@@ -3,13 +3,22 @@
     public class GildedRose
     {
         IList<Item> Items;
-        public GildedRose(IList<Item> Items)
+        public GildedRose()
         {
-            this.Items = Items;
+        }
+
+        public void SetItems(IList<Item> items)
+        {
+            this.Items = items;
         }
 
         public void UpdateQuality()
         {
+            if (Items == null)
+            {
+                throw new InvalidOperationException($"{nameof(Items)} is null.");
+            }
+            
             for (var i = 0; i < Items.Count; i++)
             {
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
--- src/GildedRoseRefactoringKata.Host/Program.cs
+++ src/GildedRoseRefactoringKata.Host/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using GildedRoseRefactoringKata.Host;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
 Console.WriteLine("Hello, World!");
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.TryAddSingleton<GildedRose>();
+    })
+    .Build();
+
+var gildedRose = host.Services.GetRequiredService<GildedRose>();
+gildedRose.UpdateQuality();
+
+await host.RunAsync();
+
+
+    
+    
